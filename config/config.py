b77831d1a0edc7ed53630d127a31f100cc887f9b
Slava096 <49392499+Slava096@users.noreply.github.com>
2021-05-23 15:19:27 +0600

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,17 @@
+#twitter api config
+consumer_key=""
+consumer_secret=""
+acess_token=""
+access_secret=""
+
+#parser config
+hrome_driver_patch = r"chromedriver.exe"
+page_load_waiting = 5 #in seconds
+
+#publishing config
+weekly_twitt_day=7 #iso format 1-Monday 7-Sunday
+weekly_twitt_hour=5 #24 hour format
+
+hourly_twitt_template_patch=r"tamplates/hourly.txt" #the path to the hourly tweet template
+weekly_twitt_template_patch=r"tamplates/weekly.txt" #the path to the hourly tweet template
+
